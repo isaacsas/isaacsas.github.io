c1277f6f762c18990c5bbe53902615a5820bb6f8
isaacsas <isaacsas@users.noreply.github.com>
2025-05-30 12:56:01 +0000

    Deploying to gh-pages from @ isaacsas/isaacsas.github.io@bbec16b0f4f3107e5268c315f719a5f022205752 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -32,7 +32,7 @@ ninja.data = [{
           },
         },{id: "nav-cv",
           title: "CV",
-          description: "Linked PDF provides a more comprehensive CV. See also my [Google Scholar](https://scholar.google.com/citations?user=Ap9PfVQAAAAJ&amp;hl=en).",
+          description: "Linked PDF provides a more comprehensive CV. See also my Google Scholar page.",
           section: "Navigation",
           handler: () => {
             window.location.href = "/cv/";
