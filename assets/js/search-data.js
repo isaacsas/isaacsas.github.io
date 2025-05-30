32ad86e761d0da9f2d775ffb79670cf6751d5b8f
isaacsas <isaacsas@users.noreply.github.com>
2025-05-30 01:48:36 +0000

    Deploying to gh-pages from @ isaacsas/isaacsas.github.io@67bf6d980dd740c6fb868d4566dad7385cd1ddaa 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -32,7 +32,7 @@ ninja.data = [{
           },
         },{id: "nav-teaching",
           title: "teaching",
-          description: "Materials for courses you taught. Replace this text with your description.",
+          description: "Please see Blackboard for course information.",
           section: "Navigation",
           handler: () => {
             window.location.href = "/teaching/";
